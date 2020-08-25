f5d15ee309f1ea010a5e4931f0b299aa4cb9d367
syarifgultom <syarifgultom02@gmail.com>
2020-08-25 09:37:47 +0700

    Tugas Intent Data With Passing

---
+++ IntentPassingData2/app/src/main/java/com/example/intentpassingdata/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.intentpassingdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+
+public class Main2Activity  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView t = (TextView) findViewById(R.id.text_viewdata);
+        Bundle bundle=getIntent().getExtras();
+        String s=bundle.getString( "name");
+        t.setText(s);
+    }
+}
